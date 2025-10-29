d828ea660d6c41f42e7f098626d6537def0cfbd9
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2025-10-29 22:28:03 +0100

    feat: create filters for coaches

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+// import from packages
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+
 // import from the libraries
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
@@ -24,13 +27,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <TRPCReactProvider>
-      <html lang="en">
-        <body className={`${inter.className} antialiased`}>
-          <Toaster />
-          {children}
-        </body>
-      </html>
-    </TRPCReactProvider>
+    <NuqsAdapter>
+      <TRPCReactProvider>
+        <html lang="en">
+          <body className={`${inter.className} antialiased`}>
+            <Toaster />
+            {children}
+          </body>
+        </html>
+      </TRPCReactProvider>
+    </NuqsAdapter>
   );
 }
--- src/modules/agents/ui/views/coaches-view.tsx
+++ src/modules/agents/ui/views/coaches-view.tsx
@@ -5,6 +5,7 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 
 // import from libraries
 import { useTRPC } from "@/trpc/client";
+import { useCoachFilters } from "@/modules/agents/hooks/use-coach-filters";
 
 // import from the components
 import { LoadingState } from "@/components/loading-state";
@@ -14,17 +15,27 @@ import { ErrorState } from "@/components/error-state";
 import { DataTable } from "@/modules/agents/ui/components/data-table";
 import { columns } from "@/modules/agents/ui/components/columns";
 import { EmptyState } from "@/components/empty-state";
+import { DataPagination } from "@/modules/agents/ui/components/data-pagination";
 
-export const AgentsView = () => {
+export const CoachesView = () => {
+  const [filters, setFilters] = useCoachFilters();
+
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  const {
+    data: { items, totalPages },
+  } = useSuspenseQuery(trpc.agents.getMany.queryOptions({ ...filters }));
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable columns={columns} data={data} />
-      {data.length === 0 && (
-      <EmptyState
-        title="Create your first coach"
+      <DataTable columns={columns} data={items} />
+      <DataPagination
+        page={filters.page}
+        totalPages={totalPages}
+        onPageChange={(page) => setFilters({ page })}
+      />
+      {items.length === 0 && (
+        <EmptyState
+          title="Create your first coach"
           description="Create a coach to join your meetings. Each coach will follow your instructions and can interact with participants during the call."
         />
       )}
@@ -32,7 +43,7 @@ export const AgentsView = () => {
   );
 };
 
-export const AgentsViewLoading = () => {
+export const CoachesViewLoading = () => {
   return (
     <LoadingState
       title="Loading agents..."
@@ -41,7 +52,7 @@ export const AgentsViewLoading = () => {
   );
 };
 
-export const AgentsViewError = () => {
+export const CoachesViewError = () => {
   return (
     <ErrorState
       title="Failed to load agents..."
