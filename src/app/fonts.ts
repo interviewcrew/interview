fc6d991033d5963bd9f2f7f217976bbf0a95e2d1
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2025-11-16 11:16:59 +0100

    feat: add monospaceneon font

---
+++ src/app/fonts.ts
@@ -0,0 +1,8 @@
+import localFont from 'next/font/local';
+
+export const monaspaceNeon = localFont({
+  src: '../../public/fonts/MonaspaceNeonVarVF[wght,wdth,slnt].f7de8168e3e86ea8ea06.woff2',
+  variable: '--font-monaspace-neon',
+  display: 'swap',
+});
+
