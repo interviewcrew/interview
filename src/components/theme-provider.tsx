f7e3e533855a38f7619535c357f8465f5b2ed109
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2025-11-23 15:40:43 +0100

    feat: update favicon on different themes (#20)
    - update favicon based on color theme
    - force system theme on mobile

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,11 +1,31 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
+import { useIsMobile } from "@/hooks/use-mobile";
+
+function ThemeWatcher() {
+  const { theme, setTheme } = useTheme();
+  const isMobile = useIsMobile();
+
+  React.useEffect(() => {
+    if (isMobile && theme !== "system") {
+      setTheme("system");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMobile, theme]);
+
+  return null;
+}
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props}>
+      <ThemeWatcher />
+      {children}
+    </NextThemesProvider>
+  );
 }
