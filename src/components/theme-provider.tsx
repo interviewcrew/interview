c0e1b9d0db955b2b0edbcbab4be9fee74f260dba
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2025-11-23 10:09:25 +0100

    feat: force system theme on mobile

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,11 +1,30 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ComponentProps, useEffect } from "react";
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
+import { useIsMobile } from "@/hooks/use-mobile";
+
+function ThemeWatcher() {
+  const { theme, setTheme } = useTheme();
+  const isMobile = useIsMobile();
+
+  useEffect(() => {
+    if (isMobile && theme !== "system") {
+      setTheme("system");
+    }
+  }, [isMobile, theme, setTheme]);
+
+  return null;
+}
 
 export function ThemeProvider({
   children,
   ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}: ComponentProps<typeof NextThemesProvider>) {
+  return (
+    <NextThemesProvider {...props}>
+      <ThemeWatcher />
+      {children}
+    </NextThemesProvider>
+  );
 }
