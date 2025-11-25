37852fc990071a60d918db8f14cf05402bc2b4ee
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2025-11-25 13:25:00 +0100

    chore: organize imports

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,4 +1,7 @@
+// import from the packages
 import { serve } from "inngest/next";
+
+// import from the libraries
 import { inngest } from "@/inngest/client";
 import { interviewsProcessing } from "@/inngest/functions";
 
